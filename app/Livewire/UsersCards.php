27d353ec0432bc3d1cfd8fd9570d76c2e9299fad
lucaslemo <lucaslemo852@gmail.com>
2024-07-05 20:27:42 -0300

    feat: users total card

--- app/Livewire/UsersCards.php
+++ app/Livewire/UsersCards.php
@@ -9,17 +9,25 @@ class UsersCards extends Component
 {
     public $usersOnline = 0;
     public $usersActive = 0;
+    public $usersTotal = 0;
 
     public function mount()
     {
         try {
             $this->usersOnline = DB::table('users')
-            ->join('sessions', 'sessions.user_id', '=', 'users.id')
-            ->distinct('users.id')
-            ->count('users.id');
+                ->join('sessions', 'sessions.user_id', '=', 'users.id')
+                ->whereNull('users.deleted_at')
+                ->distinct('users.id')
+                ->count('users.id');
 
             $this->usersActive = DB::table('users')
+                ->whereNull('users.deleted_at')
                 ->whereNotNull('users.last_login_at')
+                ->distinct('users.id')
+                ->count('users.id');
+
+            $this->usersTotal = DB::table('users')
+                ->whereNull('users.deleted_at')
                 ->distinct('users.id')
                 ->count('users.id');
         } catch (\Throwable $th) {
--- resources/views/livewire/users-cards.blade.php
+++ resources/views/livewire/users-cards.blade.php
@@ -3,5 +3,6 @@
     <div class="flex flex-row justify-between space-x-2">
         <x-card title="{{ __('Users online') }}">{{ $usersOnline }}</x-card>
         <x-card title="{{ __('Users active') }}">{{ $usersActive }}</x-card>
+        <x-card title="{{ __('Users total') }}">{{ $usersTotal }}</x-card>
     </div>
 </div>
