b7cae807d343a6d5cf667d128cfe918f6d5dc3e8
lucaslemo <lucaslemo852@gmail.com>
2024-08-21 11:54:33 -0300

    fix: database seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\App;
 
@@ -16,12 +17,24 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PermissionsSeeder::class,
-            UserSeeder::class,
             SynchronizationSeeder::class,
         ]);
 
+        if(App::isProduction()) {
+            User::create([
+                'name' => 'Lucas Admin',
+                'email' => 'lucaslemodev@gmail.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$12$ZYXd2eaSNO5HuZGghTX.9efF6HSW/AgmEtyzpk5HUCqSO5rdfXpoK',
+                'type' => 'administrator',
+                'cpf' => '06952649310',
+                'active' => true,
+            ]);
+        }
+
         if(!App::isProduction()) {
             $this->call([
+                UserSeeder::class,
                 // CustomerSeeder::class,
                 // OrderSeeder::class,
                 // InvoiceSeeder::class,
