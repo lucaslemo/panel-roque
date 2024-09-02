764d6d125480017f33508abfd95c8a2142662a9b
lucaslemo <lucaslemo852@gmail.com>
2024-09-02 18:52:00 -0300

    feat: start super admin screen

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,19 +1,13 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
+<x-admin-layout>
     <div class="py-12">
 
-        <livewire:users-cards />
+        {{-- <livewire:users-cards />
 
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <h2 class="font-semibold text-xl">{{ __('Active users') }}:</h2>
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <livewire:admin.dashboard.users-last-activity-table />
             </div>
-        </div>
+        </div> --}}
     </div>
-</x-app-layout>
+</x-admin-layout>
