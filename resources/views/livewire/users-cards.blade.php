28e4059c9bc9e5b017630c8bc03b389b1d8f5a82
lucaslemo <lucaslemo852@gmail.com>
2024-09-04 12:10:13 -0300

    fix: gap between cards

--- resources/views/livewire/users-cards.blade.php
+++ resources/views/livewire/users-cards.blade.php
@@ -1,6 +1,6 @@
-<div class="grid grid-cols-2 md:grid-cols-3 gap-8 w-full">
+<div class="grid grid-cols-2 md:grid-cols-3 gap-y-4 gap-x-4 md:gap-x-8 w-full">
     <x-card title="{{ __('Users online') }}">{{ $usersOnline }}</x-card>
     <x-card title="{{ __('Users active') }}">{{ $usersActive }}</x-card>
     <x-card title="{{ __('Users total') }}" class="hidden md:block">{{ $usersTotal }}</x-card>
-    <x-card title="{{ __('Total Users in the System') }}" class="block col-span-2 md:hidden">{{ $usersTotal }}</x-card>
+    <x-card title="{{ __('Total Users in the System') }}" class="block md:hidden col-span-2">{{ $usersTotal }}</x-card>
 </div>
