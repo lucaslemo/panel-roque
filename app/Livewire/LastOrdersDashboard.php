c030e2a4f904b6806e3a8ade347d1a369fc3d0ec
lucaslemo <lucaslemo852@gmail.com>
2024-10-16 13:21:42 -0300

    feat: dashboard responsive

--- app/Livewire/LastOrdersDashboard.php
+++ app/Livewire/LastOrdersDashboard.php
@@ -56,13 +56,13 @@ class LastOrdersDashboard extends Component
     #[On('set-order-detail')]
     public function setOrderDetail(int $id): void
     {
-        debugbar()->info($this->lastOrders);
-        $this->dispatch('open-modal', 'product-detail');
         $this->order = Order::select(['orders.*', 'customers.nmCliente'])
             ->with('orderHistories')
             ->join('customers', 'customers.idCliente', '=', 'orders.idCliente')
             ->whereIn('orders.idCliente', array_keys($this->selectedCustomers, true))
             ->findOrFail($id);
+
+        $this->dispatch('open-modal', 'product-detail');
     }
 
     public function mount()
